f97c7e50d825d14c7dab7e982e6b30e8be85a2aa
Dru Ibarra <Druwerd@gmail.com>
2016-04-09 01:25:53 -0700

    Rewrite

--- js/beer30countdown.js
+++ js/beer30countdown.js
@@ -1,5 +1,7 @@
+// Beer:30 is every Friday at 4:30 PM
+// This will count the days, hours, minutes, seconds until the next Beer:30
 
-const beer30dayOfWeek = 5;
+const beer30dayOfWeek = 5; // Friday
 const beer30Hour = 16;
 const beer30Min = 30;
 
@@ -41,75 +43,108 @@ const beer30asciiart = "<pre>\
 </pre>";
 
 // caculate the next beer friday
-dateNow = new Date();
-thisDay = dateNow.getDay();
-thisDate = dateNow.getDate();
-thisMonth = dateNow.getMonth();
-thisYear = dateNow.getFullYear();
+function timeUntilBeer30() {
+  var dateNow = new Date();
+  var dateFuture = new Date();
+  dateFuture.setHours(beer30Hour, beer30Min, 0, 0);
 
-dateFuture = new Date();
-if (thisDay == beer30dayOfWeek){
-  dateFuture = new Date(thisYear,thisMonth,thisDate,beer30Hour,beer30Min,0);
-}
-else if (thisDay < beer30dayOfWeek) {
-  daysTillBeer30 = beer30dayOfWeek - thisDay;
-  dateNow.setDate(dateNow.getDate() + daysTillBeer30);
-  thisDate = dateNow.getDate();
-  thisMonth = dateNow.getMonth();
-  thisYear = dateNow.getFullYear();
-  dateFuture = new Date(thisYear,thisMonth,thisDate,beer30Hour,beer30Min,0);
-}
-else if (thisDay > beer30dayOfWeek) {
-  daysTillBeer30 = 7 + beer30dayOfWeek - thisDay;
-  dateNow.setDate(dateNow.getDate() + daysTillBeer30);
-  thisDate = dateNow.getDate();
-  thisMonth = dateNow.getMonth();
-  thisYear = dateNow.getFullYear();
-  dateFuture = new Date(thisYear,thisMonth,thisDate,beer30Hour,beer30Min,0);
+  var currentTime = {
+    day: dateNow.getDay(),
+    date: dateNow.getDate(),
+    month: dateNow.getMonth(),
+    year: dateNow.getFullYear(),
+    milliseconds: dateNow.getTime()
+  };
+
+  if (currentTime.day != beer30dayOfWeek) {
+    var daysTillBeer30 = beer30dayOfWeek - currentTime.day;
+    if (currentTime.day > beer30dayOfWeek) { daysTillBeer30 += 7 } // Beer:30 is next week
+
+    dateFuture.setDate(currentTime.date + daysTillBeer30);
+  }
+  return dateFuture.getTime() - (new Date()).getTime(); // milliseconds between dates
 }
 
-function GetCount() {
-  dateNow = new Date();
-  thisDay = dateNow.getDay();
-  amount = dateFuture.getTime() - dateNow.getTime(); //calc milliseconds between dates
-  delete dateNow;
 
-  if (amount < 0) {
-    document.getElementById('countbox').innerHTML="Now!";
-    document.getElementById('beer_pic').innerHTML=beer30asciiart;
+function buildTimeObject(milliseconds) {
+  var days = 0;
+  var hours = 0;
+  var minutes = 0;
+  var seconds = 0;
+  const secondsInDay = 60 * 60 * 24;
+  const secondsInHour = 60 * 60;
+  const secondsInMinute = 60;
+
+  t = Math.floor(milliseconds/1000); // convert milliseconds to seconds
+
+  days = Math.floor(t/secondsInDay);
+  t = t % secondsInDay;
+
+  hours = Math.floor(t/secondsInHour);
+  t = t % secondsInHour;
+
+  minutes = Math.floor(t/secondsInMinute);
+  t = t % secondsInMinute;
+
+  seconds = Math.floor(t);
+
+  return {
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+}
+
+function pluralize(word, count) {
+  if (count === 1) {
+    return word;
+  }
+  else { 
+    return word + "s";
+  }
+}
+
+function formatTimeUnit(word, count) {
+  return count + " " + pluralize(word, count);
+}
+
+function buildOutputString(days, hours, minutes, seconds) {
+  var out = [];
+
+  if (days != 0) {
+    out.push(formatTimeUnit("day", days));
+  }
+  if (hours != 0) {
+    out.push(formatTimeUnit("hour", hours));
+  }
+  if (minutes != 0) {
+    out.push(formatTimeUnit("minute", minutes));
+  }
+  out.push(formatTimeUnit("second", seconds));
+
+  return out.join(", ");
+}
+
+function LoadCounter() {
+  var countboxContent = "";
+  var beerPicContent = beerasciiart;
+  var timeDiff = timeUntilBeer30();
+
+  if (timeDiff < 0) {
+    countboxContent = "Now!"
+    beerPicContent = beer30asciiart;
   }
   else {
-    var days=0;
-    var hours=0;
-    var mins=0;
-    var secs=0;
-    var out="";
+    var timeObj = buildTimeObject(timeDiff);
+    countboxContent = buildOutputString(timeObj.days,
+                                        timeObj.hours,
+                                        timeObj.minutes,
+                                        timeObj.seconds);
 
-    amount = Math.floor(amount/1000); //kill the "milliseconds" so just secs
-
-    days=Math.floor(amount/86400); //days
-    amount=amount%86400;
-
-    hours=Math.floor(amount/3600); //hours
-    amount=amount%3600;
-
-    mins=Math.floor(amount/60); //minutes
-    amount=amount%60;
-
-    secs=Math.floor(amount); //seconds
-
-    if(days != 0)                            {out += days +" day"+((days!=1)?"s":"")+", ";}
-    if(days != 0 || hours != 0)              {out += hours +" hour"+((hours!=1)?"s":"")+", ";}
-    if(days != 0 || hours != 0 || mins != 0) {out += mins +" minute"+((mins!=1)?"s":"")+", ";}
-    out += secs +" secs";
-
-    document.getElementById('countbox').innerHTML=out;
-    if (thisDay == beer30dayOfWeek){
-     document.getElementById('beer_pic').innerHTML=beerasciiart;
-    }
-    else {
-     document.getElementById('beer_pic').innerHTML="<img src='images/chickenthursday2.jpg'/>";
-    }
-    setTimeout("GetCount()", 1000);
+    setTimeout("LoadCounter()", 1000);
   }
+
+  document.getElementById('countbox').innerHTML = countboxContent;
+  document.getElementById('beer_pic').innerHTML = beerPicContent;
 }
